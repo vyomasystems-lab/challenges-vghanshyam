211c763c890af6e3a81301c80def3e6e8afaa57d
vghanshyam <96714537+vghanshyam@users.noreply.github.com>
2022-08-01 00:13:08 +0530

    Update test_ROPUF.py

--- level3_design/test_ROPUF.py
+++ level3_design/test_ROPUF.py
@@ -10,4 +10,12 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge
 @cocotb.test()
-async def 
+async def test_ring_oscillator_1(dut):
+    """Test for seq detection """
+
+
+    # reset
+    dut.reset.value = 1
+    dut.en.value = 1
+    dut.S0.value = 0
+    dut.S1.value = 0 
